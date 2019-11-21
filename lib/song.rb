a40c057cd3e10bf788a41743b2d6578aa466d0a4
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-11-21 16:35:30 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     def self.new_by_filename(file_name)
         file_name_pieces = file_name.split(' - ')
         song = Song.new(file_name_pieces[1])
-        song.artist = Artist.find_or_create_by_name(file_name_pieces[0])
+        song.artist_name = file_name_pieces[0]
         song.genre = file_name_pieces[2]
         song
     end
